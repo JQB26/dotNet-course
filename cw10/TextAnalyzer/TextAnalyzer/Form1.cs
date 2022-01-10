bd09a3725257f8a1e9ac092033a72e779abc74b1
Jakub Szczepanek <kubaszczepanek@interia.pl>
2022-01-10 13:37:38 +0100

    WinForms project init

---
+++ cw10/TextAnalyzer/TextAnalyzer/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextAnalyzer
+{
+    public partial class TextAnalyzer : Form
+    {
+        public TextAnalyzer()
+        {
+            InitializeComponent();
+            textBoxInput.ScrollBars = ScrollBars.Both;
+            textBox2.ScrollBars = ScrollBars.Both;
+            textBox3.ScrollBars = ScrollBars.Both;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
