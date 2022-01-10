dd1cc509b18b8ffaff7b8eccedd7d95181d6dc17
Jakub Szczepanek <kubaszczepanek@interia.pl>
2022-01-10 11:54:08 +0100

    commit

---
+++ cw05/Cw05/Cw05/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cw05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ cw05/Cw05/Cw05/StringConnector.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+namespace Cw05
+{
+    public class StringConnector
+    {
+        public string Join(string s1, string s2)
+        {
+            if (string.IsNullOrEmpty(s1) || string.IsNullOrEmpty(s2))
+            {
+                return null;
+            }
+            else
+            {
+                return s1 + s2;
+            }
+        }
+    }
+}
